2f15c5c9c7ed2c07ccb75124d3576ca023ffc4d8
Rachel T <rachelceline15@gmail.com>
2021-05-25 18:16:31 -0500

    added files

--- UFO-level-1/static/js/app_helper.js
+++ UFO-level-1/static/js/app_helper.js
@@ -6,31 +6,35 @@
 const tableData = data;
 
 // get table references where table will be inserted
-
+const tableBody = d3.select("tbody");
+// <table id="ufo-table" class="table table-striped">
 
 function buildTable(data) {
   // When the page loads, it needs to display the table
   // But if the table reloads then you may need to ensure the 
   // previous output is cleared/overwritten from scratch 
-
+  tableBody.html("");
   // Think of the class activities for generating tables
-
+  data.forEach((dataRow) => {
+    var row = tableBody.append("tr");
+    Object.values(dataRow).forEach((d) =>{
+      row.append("td").text(d);
+    });
+  });
 }
 
 function handleClick() {
 
   // Grab the datetime value from the filter
-  
+  var date = d3.select("#datetime").property("value");
 
   // grab all the table data and set to filteredData
-  
+  let filteredData = tableData;
   // Check to see if a date was entered and filter the
   // data using that date.
-
-
-  
-
-  
+  if(date) {
+    filteredData = filteredData.filter(x => x.datetime === date);
+  }
 
   // Rebuild the table using the filtered data
   // @NOTE: If no date was entered, then filteredData will
@@ -40,7 +44,8 @@ function handleClick() {
 }
 
 // Attach an event to listen for the form button that calls handleClick on a click of the filter button
-
+var button = d3.select("#filter-btn");
+button.on("click", handleClick);
 
 // Build the table when the page loads
 buildTable(tableData);
